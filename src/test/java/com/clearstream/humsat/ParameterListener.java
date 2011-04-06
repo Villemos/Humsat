dafb6b785880d11205c55ae35516ef02196f9933
villemosg <gert.villemos@logica.com>
2011-04-06 23:29:18 +0200

    Fixed minor error in configuration. Added classes for client application.

---
+++ src/test/java/com/clearstream/humsat/ParameterListener.java
@@ -0,0 +1,21 @@
+package com.clearstream.humsat;
+
+import org.apache.camel.Exchange;
+import org.apache.log4j.Logger;
+import org.hbird.exchange.type.Parameter;
+
+public class ParameterListener {
+
+	protected static Logger logger = Logger.getLogger(ParameterListener.class);
+	
+	public void process(Exchange exchange) {
+		Object pojo = exchange.getIn().getBody();
+		if (pojo instanceof Parameter) {
+			Parameter parameter = (Parameter) pojo;
+			logger.info("Parameter: " + parameter.getName() + " == " + parameter.getValue().toString());
+		}
+		else {
+			logger.error("Pojo of type '" + pojo.getClass() + "' not a Parameter.");
+		}		
+	}
+}
