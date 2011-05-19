ed7873e056fb0a1982804252c23c0a0fa4551857
TobiasTurba <mail@tturba.de>
2011-05-19 10:20:00 +0200

    Updated Humsat.java. Now you can start Humsat either via eEclipse of 'mvn camel:run'.

--- src/main/java/org/hbird/humsat/Humsat.java
+++ src/main/java/org/hbird/humsat/Humsat.java
@@ -24,7 +24,7 @@ public class Humsat {
 	public static void main(String[] args) {
 		
 		/** Read the configuration file as the first argument. If not set, then we try the default name. */
-		String assemblyFile = System.getProperty("humsat.assembly") == null ? "classpath:Humsat.xml" : System.getProperty("humsat.assembly");
+		String assemblyFile = System.getProperty("humsat.assembly") == null ? "classpath:META-INF/spring/Humsat.xml" : System.getProperty("humsat.assembly");
 		logger.info("Starting Hummingbird based on assembly file '" + assemblyFile + "'.");
 
 		new FileSystemXmlApplicationContext(assemblyFile);
